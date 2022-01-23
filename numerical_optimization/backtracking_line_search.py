c0ed7a2b43df381b979e6006332402a8099798fe
Tony-Tan <tony.face2ai@gmail.com>
2022-01-23 10:34:18 +0800

    test functions hessian matrix

--- numerical_optimization/backtracking_line_search.py
+++ numerical_optimization/backtracking_line_search.py
@@ -1,4 +1,9 @@
 # algorithm 3.1 backtracking line search
+
+import numpy as np
+import sys
+import test_functions.test_functions as test_f
+import copy
 import random
 
 
@@ -23,7 +28,25 @@ class BacktrackingLineSearch:
         return alpha
 
 
-if __name__ == '__main__':
-    bls = BacktrackingLineSearch(rho=0.7, c=0.5)
-    OF_Q = of.Quadratic
+def steepest_descent_BLS(objective_function, threshold_delta_value,  x_init=None):
+    x_sequence = []
+    delta = sys.float_info.max
+    x_k = np.array([random.random() * 2 - 1, random.random() * 2 - 1]) if x_init is None else x_init
+    bls = BacktrackingLineSearch(rho=0.9, c=1e-4)
+    f_value = objective_function(x_k)
+    while threshold_delta_value < delta:
+        gradient_k = objective_function.derivative(x_k)
+        p_k = - gradient_k
+        alpha = bls(objective_function, x_k, p_k, gradient_k, alpha=1)
+        delta = f_value
+        x_k += alpha * p_k
+        x_sequence.append(copy.deepcopy(x_k))
+        f_value = objective_function(x_k)
+        delta = np.abs(delta - f_value)
+    return x_sequence
 
+
+if __name__ == '__main__':
+    obj_f = test_f.AckleyFunction()
+    x_s = steepest_descent_BLS(obj_f, 0.0001, x_init=[1.3, -1.3])
+    obj_f.save(x_s)
--- test_functions/test_functions.py
+++ test_functions/test_functions.py
@@ -18,7 +18,12 @@ class TestFunctions2D:
     def __call__(self, *args):
         return 0
 
-    def derivative(self, *args):
+    @staticmethod
+    def derivative(*args):
+        return 0
+
+    @staticmethod
+    def Hessian_Matrix(*args):
         return 0
 
     def save(self, trajectory):
@@ -53,18 +58,23 @@ class AckleyFunction(TestFunctions2D):
     def __init__(self):
         super().__init__('Ackley Function', -5, 5, -5, 5, [[0, 0]])
 
-    def __call__(self,x_k):
+    def __call__(self, x_k):
         x, y = x_k
         return -20 * np.exp(-0.2 * np.sqrt(0.5 * (x ** 2 + y ** 2))) - np.exp(
             0.5 * (np.cos(2 * np.pi * x) + np.cos(2 * np.pi * y))) + np.e + 20
 
-    def derivative(self, x_k):
+    @staticmethod
+    def derivative(x_k):
         x, y = x_k
         part_1 = (2 * np.sqrt(2) * np.exp(-(np.sqrt(2 * (x ** 2 + y ** 2))) / 10)) / (np.sqrt(x ** 2 + y ** 2))
         part_2 = PI * np.exp(np.cos(2 * PI * x) / 2 + np.cos(2 * PI * y) / 2)
         partial_x = part_1 * x + part_2 * np.sin(2 * PI * x)
         partial_y = part_1 * y + part_2 * np.sin(2 * PI * y)
         return np.array([partial_x, partial_y])
+
+    @staticmethod
+    def Hessian_Matrix(x_k):
+        pass
 
 
 class SphereFunction(TestFunctions2D):
@@ -75,11 +85,21 @@ class SphereFunction(TestFunctions2D):
         x, y = x_k
         return x ** 2 + y ** 2
 
-    def derivative(self, x_k):
+    @staticmethod
+    def derivative(x_k):
         x, y = x_k
         partial_x = 2 * x
         partial_y = 2 * y
         return np.array([partial_x, partial_y])
+
+    @staticmethod
+    def Hessian_Matrix(x_k):
+        x, y = x_k
+        partial_xx = 2
+        partial_xy = 0
+        partial_yx = 0
+        partial_yy = 2
+        return np.array([[partial_xx, partial_xy], [partial_yx, partial_yy]])
 
 
 class RosenbrockFunction(TestFunctions2D):
@@ -90,11 +110,21 @@ class RosenbrockFunction(TestFunctions2D):
         x, y = x_k
         return 100 * (y - x ** 2) ** 2 + (1 - x) ** 2
 
-    def derivative(self, x_k):
+    @staticmethod
+    def derivative(x_k):
         x, y = x_k
-        partial_x = -400 * x * (y - x ** 2) + 2 * x - 2
+        partial_x = -400 * x * y + 400 * x ** 3 + 2 * x - 2
         partial_y = -200 * x ** 2 + 200 * y
         return np.array([partial_x, partial_y])
+
+    @staticmethod
+    def Hessian_Matrix(x_k):
+        x, y = x_k
+        partial_xx = -400 * y + 1200 * x ** 2 + 2
+        partial_xy = -400 * x
+        partial_yx = -400 * x
+        partial_yy = 200
+        return np.array([[partial_xx, partial_xy], [partial_yx, partial_yy]])
 
 
 class BealeFunction(TestFunctions2D):
@@ -105,13 +135,23 @@ class BealeFunction(TestFunctions2D):
         x, y = x_k
         return (1.5 - x + x * y) ** 2 + (2.25 - x + x * y ** 2) ** 2 + (2.625 - x + x * y ** 3) ** 2
 
-    def derivative(self, x_k):
+    @staticmethod
+    def derivative(x_k):
         x, y = x_k
         partial_x = 0.25 * (y - 1) * (
-                    8 * x * y ** 5 + 8 * x * y ** 4 + 16 * x * y ** 3 - 8 * x * y - 24 * x + 21 * y ** 2 + 39 * y + 51)
+                8 * x * y ** 5 + 8 * x * y ** 4 + 16 * x * y ** 3 - 8 * x * y - 24 * x + 21 * y ** 2 + 39 * y + 51)
         partial_y = 0.25 * x * (
-                    24 * x * y ** 5 + 16 * x * y ** 3 - 24 * x * y ** 2 - 8 * x * y - 8 * x + 63 * y ** 2 + 36 * y + 12)
+                24 * x * y ** 5 + 16 * x * y ** 3 - 24 * x * y ** 2 - 8 * x * y - 8 * x + 63 * y ** 2 + 36 * y + 12)
         return np.array([partial_x, partial_y])
+
+    @staticmethod
+    def Hessian_Matrix(x_k):
+        x, y = x_k
+        partial_xx = 0.25 * (y - 1) * (8 * y ** 5 + 8 * y ** 4 + 16 * y ** 3 - 8 * y - 24)
+        partial_xy = 12 * x * y ** 5 + 8 * x * y ** 2 - 12 * x * y ** 2 - 4 * x + 63 * y ** 2 / 4 + 9 * y + 3
+        partial_yx = 12 * x * y ** 5 + 8 * x * y ** 3 - 12 * x * y ** 2 - 4 * x + 63 * y ** 2 / 4 + 9 * y + 3
+        partial_yy = x * (60 * x * y ** 4 + 24 * x * y ** 2 - 24 * x * y - 4 * x + 63 * y + 18) / 2.
+        return np.array([[partial_xx, partial_xy], [partial_yx, partial_yy]])
 
 
 class GoldsteinPriceFunction(TestFunctions2D):
@@ -137,6 +177,35 @@ class GoldsteinPriceFunction(TestFunctions2D):
                     45368 * y ** 6 - 26568 * y ** 5 + 9720 * y ** 4 + 57384 * y ** 3 + 36864 * y ** 2 + 6120 * y + 720
         return np.array([partial_x, partial_y])
 
+    @staticmethod
+    def Hessian_Matrix(x_k):
+        x, y = x_k
+        partial_xx = 8064 * x ** 6 - 12096 * x ** 5 * y - 32256 * x ** 5 - 19440 * x ** 4 * y ** 2 + 40320 * x ** 4 * y + 28560 * x ** 4 + 24480 * x ** 3 * y ** 3 + \
+                     51840 * x ** 3 * y ** 2 - 3360 * x ** 3 * y + 26880 * x ** 3 + 15660 * x ** 2 * y ** 4 - 48960 * x ** 2 * y ** 3 - 64440 * x ** 2 * y ** 2 - \
+                     92160 * x ** 2 * y - 29448 * x ** 2 - 11016 * x * y ** 5 - 20880 * x * y ** 4 + 7440 * x * y ** 3 + 59040 * x * y ** 2 + 34704 * x * y - \
+                     6432 * x - 2916 * y ** 6 + 7344 * y ** 5 + 17460 * y ** 4 + 10080 * y ** 3 + 15552 * y ** 2 + 14688 * y + 2520
+
+        partial_xy = - 2016 * x ** 6 - 7776 * x ** 5 * y + 8064 * x ** 5 + 18360 * x ** 4 * y ** 2 + 25920 * x ** 4 * y - \
+                     840 * x ** 4 + 20880 * x ** 3 * y ** 3 - 48960 * x ** 3 * y ** 2 - 42960 * x ** 3 * y - \
+                     30720 * x ** 3 - 27540 * x ** 2 * y ** 4 - 41760 * x ** 2 * y ** 3 + 11160 * x ** 2 * y ** 2 + \
+                     59040 * x ** 2 * y + 17352 * x ** 2 - 17496 * x * y ** 5 + 36720 * x * y ** 4 + 69840 * x * y ** 3 + \
+                     30240 * x * y ** 2 + 31104 * x * y + 14688 * x + 6804 * y ** 6 + 11664 * y ** 5 - 5940 * y ** 4 - \
+                     47520 * y ** 3 - 70848 * y ** 2 - 38592 * y - 4680
+
+        partial_yx = - 2016 * x ** 6 - 7776 * x ** 5 * y + 8064 * x ** 5 + 18360 * x ** 4 * y ** 2 + 25920 * x ** 4 * y - \
+                     840 * x ** 4 + 20880 * x ** 3 * y ** 3 - 48960 * x ** 3 * y ** 2 - 42960 * x ** 3 * y - 30720 * x ** 3 - \
+                     27540 * x ** 2 * y ** 4 - 41760 * x ** 2 * y ** 3 + 11160 * x ** 2 * y ** 2 + 59040 * x ** 2 * y + \
+                     17352 * x ** 2 - 17496 * x * y ** 5 + 36720 * x * y ** 4 + 69840 * x * y ** 3 + 30240 * x * y ** 2 + \
+                     31104 * x * y + 14688 * x + 6804 * y ** 6 + 11664 * y ** 5 - 5940 * y ** 4 - 47520 * y ** 3 - 70848 * y ** 2 - 38592 * y - 4680
+
+        partial_yy = - 1296 * x ** 6 + 7344 * x ** 5 * y + 5184 * x ** 5 + 15660 * x ** 4 * y ** 2 - 24480 * x ** 4 * y - \
+                     10740 * x ** 4 - 36720 * x ** 3 * y ** 3 - 41760 * x ** 3 * y ** 2 + 7440 * x ** 3 * y + 19680 * x ** 3 - \
+                     43740 * x ** 2 * y ** 4 + 73440 * x ** 2 * y ** 3 + 104760 * x ** 2 * y ** 2 + 30240 * x ** 2 * y + \
+                     15552 * x ** 2 + 40824 * x * y ** 5 + 58320 * x * y ** 4 - 23760 * x * y ** 3 - 142560 * x * y ** 2 - \
+                     141696 * x * y - 38592 * x + 40824 * y ** 6 - 272208 * y ** 5 - 132840 * y ** 4 + 38880 * y ** 3 + 172152 * y ** 2 + 73728 * y + 6120
+
+        return np.array([[partial_xx, partial_xy], [partial_yx, partial_yy]])
+
 
 class BoothFunction(TestFunctions2D):
     def __init__(self):
@@ -152,6 +221,15 @@ class BoothFunction(TestFunctions2D):
         partial_y = 8 * x + 10 * y - 38
         return np.array([partial_x, partial_y])
 
+    @staticmethod
+    def Hessian_Matrix(x_k):
+        x, y = x_k
+        partial_xx = 10
+        partial_xy = 8
+        partial_yx = 8
+        partial_yy = 10
+        return np.array([[partial_xx, partial_xy], [partial_yx, partial_yy]])
+
 
 class MatyasFunction(TestFunctions2D):
     def __init__(self):
@@ -166,6 +244,15 @@ class MatyasFunction(TestFunctions2D):
         partial_x = (13 * x - 12 * y) / 25
         partial_y = -12 * x / 25 + 13 * y / 25
         return np.array([partial_x, partial_y])
+
+    @staticmethod
+    def Hessian_Matrix(x_k):
+        x, y = x_k
+        partial_xx = 13 / 25
+        partial_xy = - 12 / 25
+        partial_yx = -12 / 25
+        partial_yy = 13 / 25
+        return np.array([[partial_xx, partial_xy], [partial_yx, partial_yy]])
 
 
 class LeviFunctionN13(TestFunctions2D):
@@ -185,6 +272,19 @@ class LeviFunctionN13(TestFunctions2D):
                     2 * (y - 1) * (np.sin(2 * PI * y) ** 2 + 1)
         return np.array([partial_x, partial_y])
 
+    @staticmethod
+    def Hessian_Matrix(x_k):
+        x, y = x_k
+        partial_xx = 18 * PI * PI * np.cos(6 * PI * x) - np.cos(6 * PI * y) + 3
+        partial_xy = 6 * PI * (x - 1) * np.sin(6 * PI * y)
+        partial_yx = 6 * PI * (x - 1) * np.sin(6 * PI * y)
+        partial_yy = 6 * PI * (x - 1) ** 2 * (-3 * PI * np.sin(3 * PI * y) ** 2 + 3 * PI * np.cos(3 * PI * y) ** 2) + \
+                     4 * PI * (2 * y - 2) * np.sin(2 * PI * y) * np.cos(2 * PI * y) + \
+                     4 * PI * ((y - 1) ** 2 * (- 2 * PI * np.sin(2 * PI * y) ** 2 + 2 * PI * np.cos(2 * PI * y) ** 2) +
+                               2 * (y - 1) * np.sin(2 * PI * y) * np.cos(2 * PI * y)) + 2 * np.sin(2 * PI * y) ** 2 + 2
+
+        return np.array([[partial_xx, partial_xy], [partial_yx, partial_yy]])
+
 
 class HimmelblausFunction(TestFunctions2D):
     def __init__(self):
@@ -202,10 +302,19 @@ class HimmelblausFunction(TestFunctions2D):
         partial_y = 2 * x ** 2 + 4 * y * (x + y ** 2 - 7) + 2 * y - 22
         return np.array([partial_x, partial_y])
 
+    @staticmethod
+    def Hessian_Matrix(x_k):
+        x, y = x_k
+        partial_xx = 12 *x**2 + 4*y -42
+        partial_xy = 4*(x+y)
+        partial_yx = 4*(x+y)
+        partial_yy = 4*x + 12*y**2 -26
+        return np.array([[partial_xx, partial_xy], [partial_yx, partial_yy]])
 
-class Three_HumpCamelFunction(TestFunctions2D):
+
+class ThreeHumpCamelFunction(TestFunctions2D):
     def __init__(self):
-        super(Three_HumpCamelFunction, self).__init__('Three-Hump Camel Function', -5, 5, -5, 5, [[0, 0]])
+        super(ThreeHumpCamelFunction, self).__init__('Three-Hump Camel Function', -5, 5, -5, 5, [[0, 0]])
 
     def __call__(self, x_k):
         x, y = x_k
@@ -216,6 +325,15 @@ class Three_HumpCamelFunction(TestFunctions2D):
         partial_x = -21 * x ** 3 / 5 + 13 * x / 3 + y
         partial_y = x + 2 * y
         return np.array([partial_x, partial_y])
+
+    @staticmethod
+    def Hessian_Matrix(x_k):
+        x, y = x_k
+        partial_xx = 13/3 - 63*x**2/5
+        partial_xy = 1
+        partial_yx = 1
+        partial_yy = 2
+        return np.array([[partial_xx, partial_xy], [partial_yx, partial_yy]])
 
 
 class EasomFunction(TestFunctions2D):
@@ -232,6 +350,17 @@ class EasomFunction(TestFunctions2D):
         partial_y = (-2 * (PI - y) * np.cos(y) + np.sin(y)) * np.exp(-(x - PI) ** 2 - (y - PI) ** 2) * np.cos(x)
         return np.array([partial_x, partial_y])
 
+    @staticmethod
+    def Hessian_Matrix(x_k):
+        x, y = x_k
+        partial_xx = (-2*(PI-x)*(2*(PI-x)*np.cos(x)-np.sin(x)) +
+                      2*(PI-x)*np.sin(x)+3*np.cos(x))*np.exp(-(x-PI)**2 - (y-PI)**2)*np.cos(y)
+        partial_xy = (2*(PI-x)*np.cos(x)-np.sin(x))*(-2*(PI-y)*np.cos(y)+np.sin(y))*np.exp(-(x-PI)**2-(y-PI)**2)
+        partial_yx = (2*(PI-x)*np.cos(x)+np.sin(x))*(2*(PI-y)*np.cos(y)-np.sin(y))*np.exp(-(x-PI)**2-(y-PI)**2)
+        partial_yy = (-2*(PI-y)*(2*(PI-y)*np.cos(y)-np.sin(y)) +
+                      2*(PI-y)*np.sin(y)+3*np.cos(y))*np.exp(-(x-PI)**2 - (y-PI)**2)*np.cos(x)
+        return np.array([[partial_xx, partial_xy], [partial_yx, partial_yy]])
+
 
 class McCormickFunction(TestFunctions2D):
     def __init__(self):
@@ -247,6 +376,15 @@ class McCormickFunction(TestFunctions2D):
         partial_y = -2 * x + 2 * y + np.cos(x + y) + 2.5
         return np.array([partial_x, partial_y])
 
+    @staticmethod
+    def Hessian_Matrix(x_k):
+        x, y = x_k
+        partial_xx = 2 - np.sin(x + y)
+        partial_xy = -np.sin(x+y) - 2
+        partial_yx = -np.sin(x+y) - 2
+        partial_yy = 2 - np.sin(x + y)
+        return np.array([[partial_xx, partial_xy], [partial_yx, partial_yy]])
+
 
 class StyblinskiTangFunction(TestFunctions2D):
     def __init__(self):
@@ -261,6 +399,15 @@ class StyblinskiTangFunction(TestFunctions2D):
         partial_x = 2 * x ** 3 - 16 * x + 2.5
         partial_y = 2 * y ** 3 - 16 * y + 2.5
         return np.array([partial_x, partial_y])
+
+    @staticmethod
+    def Hessian_Matrix(x_k):
+        x, y = x_k
+        partial_xx = 6 * x ** 2 - 16
+        partial_xy = 0
+        partial_yx = 0
+        partial_yy = 6 * y ** 2 - 16
+        return np.array([[partial_xx, partial_xy], [partial_yx, partial_yy]])
 
 
 if __name__ == '__main__':
